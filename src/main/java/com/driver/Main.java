45c8ca9d20765c041835357ffca1f804a17342cb
VISVANATH123 <viswa3643@gmail.com>
2025-07-13 16:46:38 +0530

    helllow

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,34 @@
 package com.driver;
 
 public class Main {
-    public static class product{
-        public int product(int x,int y){
-            return x*y;
-        }
-        public int product(int x,int y,int z){
-            return x*y*z;
-        }
-        public double product(double x,double y) {
+
+    // Task 1: Create a class Product inside Main class
+    static class Product {
+
+        // Task 3: Method with 2 int parameters
+        public int product(int x, int y) {
             return x * y;
         }
-    }//
-    public static void main(String args[]){
-        product p=new product();
-        System.out.println(p.product(5,4));
-        System.out.println(p.product(7,7,8));
-        System.out.println(p.product(9.00,8.76));
+
+        // Task 4: Overloaded method with 3 int parameters
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        // Task 5: Overloaded method with 2 double parameters
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
-    //completed
+
+    public static void main(String[] args) {
+
+        // Task 2: Create object of Product
+        Product p = new Product();
+
+        // Call each overloaded method
+        System.out.println("Product of 2 int: " + p.product(2, 3));
+        System.out.println("Product of 3 int: " + p.product(2, 3, 4));
+        System.out.println("Product of 2 double: " + p.product(2.5, 3.5));
+    }
 }
